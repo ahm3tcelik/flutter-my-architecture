c49ae8890ae7f6f0108a7edbacf81402a5ecf482
ahm3tcelik <ahm3tcelik72@gmail.com>
2021-02-11 15:50:51 +0300

    fix user sembastdao

--- lib/app/data/data_sources/local/concrete/sembast/user_sources/user_sembast_local_datasrc.dart
+++ lib/app/data/data_sources/local/concrete/sembast/user_sources/user_sembast_local_datasrc.dart
@@ -1,5 +1,6 @@
 import 'package:sembast/sembast.dart';
 import 'package:template/app/data/models/user.dart';
+import 'package:template/core/data_sources/local/IDao.dart';
 import 'package:template/core/data_sources/local/IDbProvider.dart';
 import 'package:template/core/data_sources/local/sembast/ISembastDao.dart';
 import 'package:template/core/data_sources/local/sembast/base_sembast_data_source.dart';
@@ -17,7 +18,7 @@ class UserSembastDataSource extends BaseSembastDataSource<User>
 
   @override
   Future<List<User>> searchByName(String key) async {
-    final folder = intMapStoreFactory.store(dao.mainStoreRef);
+    final folder = intMapStoreFactory.store((dao as ISembastDao).mainStoreRef);
     final finder = Finder(filter: Filter.matches('userName', key));
     final result = await folder.find(await dbProvider.getDb(), finder: finder);
 

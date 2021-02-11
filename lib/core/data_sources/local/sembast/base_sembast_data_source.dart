ba575086b8cfc90027639ec0481181453ebea9ba
ahm3tcelik <ahm3tcelik72@gmail.com>
2021-02-11 15:41:01 +0300

    fix sembastdao

--- lib/core/data_sources/local/sembast/base_sembast_data_source.dart
+++ lib/core/data_sources/local/sembast/base_sembast_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:sembast/sembast.dart';
+import 'package:template/core/data_sources/local/IDao.dart';
 import 'package:template/core/data_sources/local/sembast/ISembastDao.dart';
 import 'package:template/core/models/IEntity.dart';
 import '../IDbProvider.dart';
@@ -10,18 +11,18 @@ class BaseSembastDataSource<T extends IEntity<T>>
 
   const BaseSembastDataSource(this.dbProvider, this.dao);
 
-  final ISembastDao<T> dao;
+  final IDao<T> dao;
 
   @override
   Future<int> add(T ent) async {
-    final folder = intMapStoreFactory.store(dao.mainStoreRef);
+    final folder = intMapStoreFactory.store((dao as ISembastDao).mainStoreRef);
     final result = await folder.add(await dbProvider.getDb(), ent.toJson());
     return result;
   }
 
   @override
   Future<int> delete(id, T ent) async {
-    final folder = intMapStoreFactory.store(dao.mainStoreRef);
+    final folder = intMapStoreFactory.store((dao as ISembastDao).mainStoreRef);
     final finder = Finder(filter: Filter.equals(dao.primaryKey, id));
     final result =
         await folder.delete(await dbProvider.getDb(), finder: finder);
@@ -30,7 +31,7 @@ class BaseSembastDataSource<T extends IEntity<T>>
 
   @override
   Future<T> get(id) async {
-    final folder = intMapStoreFactory.store(dao.mainStoreRef);
+    final folder = intMapStoreFactory.store((dao as ISembastDao).mainStoreRef);
     final finder = Finder(filter: Filter.equals(dao.primaryKey, id));
     final result =
         await folder.findFirst(await dbProvider.getDb(), finder: finder);
@@ -39,7 +40,7 @@ class BaseSembastDataSource<T extends IEntity<T>>
 
   @override
   Future<List<T>> getAll() async {
-    final folder = intMapStoreFactory.store(dao.mainStoreRef);
+    final folder = intMapStoreFactory.store((dao as ISembastDao).mainStoreRef);
     final result = await folder.find(await dbProvider.getDb());
 
     return result
@@ -49,14 +50,14 @@ class BaseSembastDataSource<T extends IEntity<T>>
 
   @override
   Future<void> putFromRemote(List<T> entList) async {
-    final folder = intMapStoreFactory.store(dao.mainStoreRef);
+    final folder = intMapStoreFactory.store((dao as ISembastDao).mainStoreRef);
     folder.delete(await dbProvider.getDb());
     folder.addAll(await dbProvider.getDb(), entList.map((e) => e.toJson()).toList());
   }
 
   @override
   Future<int> update(id, T ent) async {
-    final folder = intMapStoreFactory.store(dao.mainStoreRef);
+    final folder = intMapStoreFactory.store((dao as ISembastDao).mainStoreRef);
     final finder = Finder(filter: Filter.equals(dao.primaryKey, id));
     final result = await folder.update(await dbProvider.getDb(), ent.toJson(),
         finder: finder);
